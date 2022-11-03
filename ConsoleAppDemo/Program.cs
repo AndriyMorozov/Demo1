d8a41510e7984a9a058d31eabc0be1cca997db9a
Andrii Morozov <morozov@ztu.edu.ua>
2022-11-03 19:14:04 +0200

    changed language

--- ConsoleAppDemo/Program.cs
+++ ConsoleAppDemo/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello!");
-Console.WriteLine("Hello!");
-Console.WriteLine("Hello!");
-Console.WriteLine("Hello!");
+Console.WriteLine("Привіт!");
+Console.WriteLine("Привіт!");
+Console.WriteLine("Привіт!");
+Console.WriteLine("Привіт!");
